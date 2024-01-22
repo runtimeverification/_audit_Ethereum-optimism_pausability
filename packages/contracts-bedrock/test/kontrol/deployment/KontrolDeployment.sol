a7254a650f0300e18f4b374bdd6183842b388688
Juan C <juanconrod@protonmail.com>
2024-01-22 13:20:45 +0100

    `KontrolDeployment`: properly update deployment process and file

--- packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
+++ packages/contracts-bedrock/test/kontrol/deployment/KontrolDeployment.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import { Deploy } from "scripts/Deploy.s.sol";
+import { StandardBridge } from "src/universal/StandardBridge.sol";
 
 contract KontrolDeployment is Deploy {
     function runKontrolDeployment() public stateDiff {
@@ -12,15 +13,22 @@ contract KontrolDeployment is Deploy {
         deployERC1967Proxy("OptimismPortalProxy");
         deployERC1967Proxy("L2OutputOracleProxy");
         deployERC1967Proxy("SystemConfigProxy");
+        deployL1StandardBridgeProxy();
+        deployL1CrossDomainMessengerProxy();
         transferAddressManagerOwnership(); // to the ProxyAdmin
 
         // deployImplementations();
         deployOptimismPortal();
+        deployL1CrossDomainMessenger();
         deployL2OutputOracle();
         deploySystemConfig();
+        deployL1StandardBridge();
 
         // initializeImplementations();
         initializeSystemConfig();
+        initializeL1StandardBridge();
+        initializeL1CrossDomainMessenger();
+        initializeL2OutputOracle();
         initializeOptimismPortal();
     }
 }
